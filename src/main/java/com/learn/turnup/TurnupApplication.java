d4c3cd0e22af5edb8f84be1c295164fe88733f1f
Pavel <zacikp@seznam.cz>
2025-06-03 14:58:52 +0200

    configs

---
+++ src/main/java/com/learn/turnup/TurnupApplication.java
@@ -0,0 +1,13 @@
+package com.learn.turnup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurnupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurnupApplication.class, args);
+	}
+
+}
