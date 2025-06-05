20266b208441b6bf1c81950097badf0092734389
Pavel <zacikp@seznam.cz>
2025-06-05 15:03:34 +0200

    change ID type inAppUser repository

--- src/main/java/com/learn/turnup/repositories/AppUserRepository.java
+++ src/main/java/com/learn/turnup/repositories/AppUserRepository.java
@@ -2,7 +2,8 @@ package com.learn.turnup.repositories;
 
 import com.learn.turnup.entities.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 
-public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+import java.util.UUID;
+
+public interface AppUserRepository extends JpaRepository<AppUser, UUID> {
 }
