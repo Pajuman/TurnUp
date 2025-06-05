6336de7a3b466a16a0db95499aa8dbd91d7db2c7
Pavel <zacikp@seznam.cz>
2025-06-05 15:02:52 +0200

    add methods to JPA for Word and Lesson

--- src/main/java/com/learn/turnup/repositories/LessonRepository.java
+++ src/main/java/com/learn/turnup/repositories/LessonRepository.java
@@ -2,7 +2,12 @@ package com.learn.turnup.repositories;
 
 import com.learn.turnup.entities.Lesson;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 
-public interface LessonRepository extends JpaRepository<Lesson, Integer> {
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface LessonRepository extends JpaRepository<Lesson, UUID> {
+    Optional<List<Lesson>> findAllByAppUserId(UUID xUserId);
+
 }
--- src/main/java/com/learn/turnup/repositories/WordRepository.java
+++ src/main/java/com/learn/turnup/repositories/WordRepository.java
@@ -1,11 +1,14 @@
 package com.learn.turnup.repositories;
 
+import com.learn.turnup.entities.Lesson;
 import com.learn.turnup.entities.Word;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
-public interface WordRepository extends JpaRepository<Word, Long> {
+public interface WordRepository extends JpaRepository<Word, UUID> {
     Optional<Word> findByQuestionAndAnswer(String question, String answer);
+    Optional<List<Word>> findAllByLesson(Lesson lesson);
 }
