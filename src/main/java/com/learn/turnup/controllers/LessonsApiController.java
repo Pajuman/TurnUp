cd740d10dceee802be37dce3042d7e860297adbc
Pavel <zacikp@seznam.cz>
2025-06-05 15:00:53 +0200

    add services to controllers

--- src/main/java/com/learn/turnup/controllers/LessonsApiController.java
+++ src/main/java/com/learn/turnup/controllers/LessonsApiController.java
@@ -7,6 +7,8 @@ import com.learn.turnup.dto.LessonDTO;
 import com.learn.turnup.dto.NewLessonDTO;
 import com.learn.turnup.dto.NewWordDTO;
 import com.learn.turnup.dto.WordDTO;
+import com.learn.turnup.services.LessonsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,36 +18,38 @@ import jakarta.annotation.Generated;
 @Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2025-06-03T14:15:17.222973100+02:00[Europe/Prague]", comments = "Generator version: 7.9.0")
 @Controller
 @RequestMapping("${openapi.word.base-path:/api}")
+@RequiredArgsConstructor
 public class LessonsApiController implements LessonsApi {
 
+    private final LessonsService lessonsService;
 
     @Override
     public ResponseEntity<Void> copySharedLesson(UUID xUserId, UUID lessonId) {
-        return null;
+        return lessonsService.copySharedLesson(xUserId, lessonId);
     }
 
     @Override
     public ResponseEntity<Void> createLesson(UUID xUserId, NewLessonDTO newLessonDTO) {
-        return null;
+        return lessonsService.createLesson(xUserId, newLessonDTO);
     }
 
     @Override
     public ResponseEntity<Void> createWord(UUID xUserId, UUID lessonId, List<NewWordDTO> newWordDTO) {
-        return null;
+        return lessonsService.createWords(xUserId, lessonId, newWordDTO);
     }
 
     @Override
     public ResponseEntity<Void> deleteLesson(UUID xUserId, UUID lessonId) {
-        return null;
+        return lessonsService.deleteLesson(xUserId, lessonId);
     }
 
     @Override
     public ResponseEntity<List<WordDTO>> getWordsByLessonId(UUID xUserId, UUID lessonId) {
-        return null;
+        return lessonsService.getWordsByLessonId(xUserId, lessonId);
     }
 
     @Override
     public ResponseEntity<Void> updateLesson(UUID xUserId, LessonDTO lessonDTO) {
-        return null;
+        return lessonsService.updateLesson(xUserId, lessonDTO);
     }
 }
