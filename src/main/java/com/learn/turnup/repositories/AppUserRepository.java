bbdfb2b6b384e3a0b1d91c6bf3fd00f0e3ef56e3
Pavel <zacikp@seznam.cz>
2025-06-09 08:53:25 +0200

    add method headers to repository

--- src/main/java/com/learn/turnup/repositories/AppUserRepository.java
+++ src/main/java/com/learn/turnup/repositories/AppUserRepository.java
@@ -3,8 +3,11 @@ package com.learn.turnup.repositories;
 import com.learn.turnup.entities.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface AppUserRepository extends JpaRepository<AppUser, UUID> {
-    AppUser findByAppUserNameAndPasswordHash(String username, String password);
+    Optional<AppUser> findByAppUserNameAndPasswordHash(String username, String password);
+    Boolean existsByAppUserName(String username);
+    Optional<AppUser> findByAppUserName(String username);
 }
