f704b70c9ade3ac5ff0e3df141556dadd07708c9
Pavel <zacikp@seznam.cz>
2025-06-24 13:11:02 +0200

    app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { LessonsOverviewComponent } from './lessons-overview/lessons-overview.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [LessonsOverviewComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  standalone: true,
 })
 export class AppComponent {
   title = 'turnup';
