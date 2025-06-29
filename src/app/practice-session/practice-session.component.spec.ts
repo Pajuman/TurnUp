e37229b2a4dd3a38e924a8fc3185c2ab8a8d0ae0
Pavel <pavelzacik@gmail.com>
2025-06-29 08:46:23 +0200

    add components for practising words

---
+++ src/app/practice-session/practice-session.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PracticeSessionComponent } from './practice-session.component';
+
+describe('PracticeSessionComponent', () => {
+  let component: PracticeSessionComponent;
+  let fixture: ComponentFixture<PracticeSessionComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PracticeSessionComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(PracticeSessionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/practice-session/practice-session.component.ts
@@ -0,0 +1,126 @@
+import {Component, input, InputSignal, OnInit, Signal, signal, WritableSignal} from '@angular/core';
+import {CardComponent} from '../card/card.component';
+import {Button} from 'primeng/button';
+import {WordDTO} from '../api';
+import {log} from '@angular-devkit/build-angular/src/builders/ssr-dev-server';
+
+@Component({
+  selector: 'practice-session',
+  imports: [
+    CardComponent,
+    Button
+  ],
+  templateUrl: './practice-session.component.html',
+  styleUrl: './practice-session.component.scss'
+})
+export class PracticeSessionComponent implements OnInit {
+  public readonly words: InputSignal<WordDTO[]> = input.required();
+  public currentCount = signal(0);
+  public practiceCount = signal(20);
+  private separatedWords: WordDTO[][] = [[],[],[],[],[],[]];
+  private leftBorder = 0;
+  private rightBorder = 5;
+  private spread = 0;
+  private generatedMax = 0;
+  public currentWord: WordDTO = {} as WordDTO;
+  private currentWordArrayIndex = 0;
+  private currentWordIndex = 0;
+  private previousWords: string[] = [];
+
+  ngOnInit(): void {
+this.separateWords();
+this.nextWord();
+  }
+
+
+  public correct(){
+    if(this.currentWord.score < 5){
+      ++this.currentWord.score;
+
+      this.separatedWords[this.currentWordArrayIndex + 1].push(this.currentWord)
+      this.separatedWords[this.currentWordArrayIndex].splice(this.currentWordIndex, 1);
+    }
+    this.nextWord();
+  }
+
+  public wrong(){
+    if(this.currentWord.score > 0){
+      --this.currentWord.score;
+      this.separatedWords[this.currentWordArrayIndex - 1].push(this.currentWord)
+      this.separatedWords[this.currentWordArrayIndex].splice(this.currentWordIndex, 1);
+    }
+    this.nextWord();
+  }
+
+  public back(){}
+
+  private nextWord(){
+    this.setBothBorders();
+    this.spread = this.rightBorder - this.leftBorder + 1;
+    this.setNewCurrentWord();
+    this.currentCount.update((value) => value + 1);
+    if(this.currentCount() === this.practiceCount()){
+    console.log('koneeec')
+    }
+  }
+
+  private separateWords(){
+    this.words().forEach((word) => {
+      this.separatedWords[word.score].push(word);
+    });
+  }
+
+  private setBothBorders(){
+    this.leftBorder = 0;
+    this.setBorder(0, 'plus')
+    this.rightBorder = 5;
+    this.setBorder(5, 'minus')
+  }
+
+  private setBorder(index:number, operator: 'plus' | 'minus'){
+    if(this.separatedWords[index].length === 0){
+      if(operator === 'plus'){
+        this.setBorder(++this.leftBorder, operator);
+      }
+      else{
+        this.setBorder(--this.rightBorder, operator);
+      }
+    }
+    else{return;}
+  }
+
+  private getRandomInt(arrLength:number, min = 0){
+    return Math.floor(Math.random() * (arrLength - min)) + min;
+  }
+
+  private setNewCurrentWord(){
+    this.currentWord = this.generateSemiRandomWord();
+    if(this.previousWords.includes(this.currentWord.id)){
+      this.currentWord = this.generateSemiRandomWord()
+    }
+    else{this.previousWords.push(this.currentWord.id)}
+    if(this.previousWords.length > 2){
+      this.previousWords.shift();
+    }
+  }
+
+  private generateSemiRandomWord(){
+    this.generatedMax = -1;
+    for(let i = this.spread; i > 0; i--){
+      const arrIndex = this.leftBorder + this.spread - i;
+      this.generatedMax += (this.separatedWords[arrIndex].length * i);
+    }
+    let generatedNumber = this.getRandomInt(this.generatedMax);
+    for(let i = this.spread; i > 0; i--){
+      this.currentWordArrayIndex = this.leftBorder + this.spread - i;
+      const numsInArr = this.separatedWords[this.currentWordArrayIndex].length * i;
+      if(generatedNumber < numsInArr ){
+        this.currentWordIndex = generatedNumber % this.separatedWords[this.currentWordArrayIndex].length;
+       return (this.separatedWords[this.currentWordArrayIndex][this.currentWordIndex]);
+      }
+      generatedNumber -= numsInArr;
+    }
+    return {} as WordDTO;
+  }
+}
+
