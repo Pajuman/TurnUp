0fda168e3563acd710e0c15058957e8cd01e0196
Pavel <zacikp@seznam.cz>
2025-06-29 16:39:19 +0200

    add words overview

---
+++ src/app/words-overview/words-overview.component.ts
@@ -0,0 +1,150 @@
+import {
+  Component,
+  input,
+  InputSignal,
+  OnInit,
+  Signal,
+  signal,
+  viewChild,
+  WritableSignal,
+} from '@angular/core';
+import { TableModule } from 'primeng/table';
+import { SelectButton } from 'primeng/selectbutton';
+import { TogglerComponent } from '../toggler/toggler.component';
+import {
+  PRACTICE_COUNT_OPTIONS,
+  SwitcherOption,
+  WORDS,
+} from '../constants/constants';
+import { FormsModule } from '@angular/forms';
+import { LessonDTO, WordDTO } from '../api';
+import {
+  ActionDialogOutput,
+  ConfirmDialogOutput,
+  Option,
+  PracticeCountOption,
+  Word,
+} from '../interfaces/interfaces';
+import { Button } from 'primeng/button';
+import { ActionsPopoverComponent } from '../actions-popover/actions-popover.component';
+import { ActionDialogComponent } from '../action-dialog/action-dialog.component';
+import { ConfirmDialogComponent } from '../confirm-dialog/confirm-dialog.component';
+
+@Component({
+  selector: 'words-overview',
+  imports: [
+    TableModule,
+    SelectButton,
+    TogglerComponent,
+    FormsModule,
+    Button,
+    ActionsPopoverComponent,
+    ConfirmDialogComponent,
+    ActionDialogComponent,
+  ],
+  templateUrl: './words-overview.component.html',
+  styleUrl: './words-overview.component.scss',
+  standalone: true,
+})
+export class WordsOverviewComponent implements OnInit {
+  public readonly lesson: InputSignal<LessonDTO> = input.required();
+  public words: WritableSignal<Word[]> = signal([]);
+  public practiceCount: WritableSignal<PracticeCountOption> = signal({
+    label: '20',
+    value: 20,
+  });
+  protected readonly SWITCHER_OPTION = SwitcherOption;
+  protected readonly PRACTICE_COUNT_OPTIONS = PRACTICE_COUNT_OPTIONS;
+  private editedWord?: Word;
+  private readonly actionDialog: Signal<ActionDialogComponent | undefined> =
+    viewChild('actionDialog');
+  private readonly confirmDialog: Signal<ConfirmDialogComponent | undefined> =
+    viewChild('confirmDialog');
+
+  public ngOnInit(): void {
+    //tady bude BE call
+    const fromBE = WORDS;
+    this.setWordsFromWordsDto(fromBE);
+  }
+
+  public togglerSwitched(event: any) {
+    console.log(event);
+  }
+
+  public practiceLesson() {
+    console.log(`jdu zkoušet ${this.lesson().lessonName}`);
+  }
+
+  public saveWordChanges(event: ActionDialogOutput) {
+    if (event.action === 'New') {
+      this.addWord(event.question, event.answer);
+    } else if (
+      event.action === 'Edit' &&
+      (event.question !== this.editedWord?.question ||
+        event.answer !== this.editedWord.answer)
+    ) {
+      this.editWord(event.question, event.answer);
+    }
+  }
+
+  public saveAllChanges() {
+    console.log('save all changes');
+  }
+
+  public wordActionSelected(actionSelected: Option, row: Word) {
+    if (actionSelected.value === 'Edit') {
+      this.editedWord = row;
+      this.openDialog('Edit', row);
+    } else if (actionSelected.value === 'Delete') {
+      row.status = 'deleted';
+    }
+  }
+
+  public openDialog(action: 'New' | 'Edit', currentWord: Word | null = null) {
+    if (action === 'Edit' && currentWord) {
+      this.actionDialog()!.question.set(currentWord.question);
+      this.actionDialog()!.answer.set(currentWord.answer);
+    }
+
+    this.actionDialog()!.action.set(action);
+    this.actionDialog()!.visible.set(true);
+  }
+
+  public back() {
+    console.log('jdu zpět');
+  }
+
+  public confirmDeletion(confirmDialogOutput: ConfirmDialogOutput) {
+    console.log(confirmDialogOutput);
+  }
+
+  private setWordsFromWordsDto(wordsDto: WordDTO[]) {
+    wordsDto.forEach((wordDto) =>
+      this.words.set([...this.words(), { ...wordDto, status: null }]),
+    );
+  }
+
+  private addWord(question: string, answer: string) {
+    const newWord: Word = {
+      id: Math.random().toString(),
+      question: question,
+      answer: answer,
+      score: 0,
+      status: 'new',
+    };
+    this.words.set([newWord, ...this.words()]);
+  }
+
+  private editWord(question: string, answer: string) {
+    if (this.editedWord) {
+      this.editedWord.question = question;
+      this.editedWord.answer = answer;
+      this.editedWord.status = 'edited';
+    }
+  }
+
+  private openConfirmDialog() {
+    this.confirmDialog()?.item.set('Word');
+    this.confirmDialog()?.visible.set(true);
+  }
+}
