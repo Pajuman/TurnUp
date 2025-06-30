4ce47b75a1114676c95ab135cfa3c13edcf00782
Pavel <zacikp@seznam.cz>
2025-06-30 08:02:13 +0200

    refactor toggler

--- src/app/features/toggler/toggler.component.ts
+++ src/app/features/toggler/toggler.component.ts
@@ -3,13 +3,16 @@ import { ToggleSwitch } from 'primeng/toggleswitch';
 import { FormsModule } from '@angular/forms';
 import { NgClass, NgOptimizedImage } from '@angular/common';
 import { LessonService } from '../../services/lesson.service';
+import { SelectButton } from 'primeng/selectbutton';
+import { PRACTICE_COUNT_OPTIONS } from '../../constants-interfaces/constants';
 
 @Component({
   selector: 'toggler',
-  imports: [ToggleSwitch, FormsModule, NgClass, NgOptimizedImage],
+  imports: [ToggleSwitch, FormsModule, NgClass, NgOptimizedImage, SelectButton],
   templateUrl: './toggler.component.html',
   styleUrl: './toggler.component.scss',
 })
 export class TogglerComponent {
   public readonly lessonService = inject(LessonService);
+  protected readonly PRACTICE_COUNT_OPTIONS = PRACTICE_COUNT_OPTIONS;
 }
