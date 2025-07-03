4f4488e53784bde192a71f85480496cbe884e562
Pavel <zacikp@seznam.cz>
2025-07-03 19:16:27 +0200

    forbid user Demo changes and distinguish only shared lessons

---
+++ src/main/java/com/learn/turnup/controllers/DebugController.java
@@ -0,0 +1,13 @@
+package com.learn.turnup.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DebugController {
+  @GetMapping("/api/ping")
+  public String ping() {
+    return "pong";
+  }
+}
