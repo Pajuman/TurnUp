98b17554feeadf1ddf3289ac68a1482c2c879be1
Pavel <zacikp@seznam.cz>
2025-08-19 11:09:47 +0200

    change status response when action for Demo user

--- backend/src/main/java/com/learn/turnup/exceptions/DefaultUser.java
+++ backend/src/main/java/com/learn/turnup/exceptions/DefaultUser.java
@@ -1,12 +1,15 @@
 package com.learn.turnup.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
 import java.util.Set;
 import java.util.UUID;
 
 public class DefaultUser {
   public static void denyForDefaultUser(UUID xUserId){
     if (xUserId.equals(DEFAULT_USER_ID)) {
-      throw new IllegalArgumentException("Not allowed for user Demo.");
+      throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Not allowed for user Demo.");
     }
   }
   private static final UUID DEFAULT_USER_ID = UUID.fromString("5193a512-1006-4136-b6dd-def65658da9d");
